7edcf078e2f7591bb071804e9b87973be5fc4e5d
Aditya <35457393+epostdit@users.noreply.github.com>
2020-04-05 22:14:16 +0700

    membuat menu ke 2

--- app/src/main/java/com/example/tugasakb/MainActivity.kt
+++ app/src/main/java/com/example/tugasakb/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
          class MainActivity : AppCompatActivity() {
-             private lateinit var button:  Button
+             private lateinit var button: Button
              override fun onCreate(savedInstanceState: Bundle?) {
                  super.onCreate(savedInstanceState)
                  setContentView(R.layout.activity_main)
