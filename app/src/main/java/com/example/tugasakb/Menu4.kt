00987cd605262bd772c5475f7faa09aed2c6a5fc
Aditya <35457393+epostdit@users.noreply.github.com>
2020-04-06 19:38:24 +0700

    mengedit menu 1,2,3 dan membuat menu 4

---
+++ app/src/main/java/com/example/tugasakb/Menu4.kt
@@ -0,0 +1,12 @@
+package com.example.tugasakb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu4 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu4)
+    }
+}
