06440450ba0acfa4226b7cff1519c44aa3485be6
Aditya <35457393+epostdit@users.noreply.github.com>
2020-04-06 10:07:03 +0700

    mengedit menu2

---
+++ app/src/main/java/com/example/tugasakb/Menu3.kt
@@ -0,0 +1,12 @@
+package com.example.tugasakb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu3)
+    }
+}
