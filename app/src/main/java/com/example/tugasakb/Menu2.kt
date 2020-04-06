f6d5c9708c65ddc9ad4bf37b162949301d79017d
Aditya <35457393+epostdit@users.noreply.github.com>
2020-04-06 10:45:01 +0700

    membuat menu 3

--- app/src/main/java/com/example/tugasakb/Menu2.kt
+++ app/src/main/java/com/example/tugasakb/Menu2.kt
@@ -1,12 +1,18 @@
 package com.example.tugasakb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Menu2 : AppCompatActivity() {
-
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu2)
+        button = findViewById(R.id.Button2)
+        button.setOnClickListener {
+            startActivity(Intent(this, Menu3::class.java))
+        }
     }
 }
